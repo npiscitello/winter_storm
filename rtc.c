031a0070b62937869910b8d252a0ea60512f944e
Nick <npiscitello@thelittlefish.net>
2019-01-24 00:05:43 -0500

    error handling, not that we need it yet

--- rtc.c
+++ rtc.c
@@ -1,14 +1,19 @@
+// see rtc.h for complete documentation
+
 #include "rtc.h"
 
 void rtc_init(void) {
-  // set to 24 hour mode
+  // don't forget to set TWI pullups
+  // make sure the RTC is in 24 hour mode
+  // if it's in 12 hour mode, get the hour 
+  // value, convert, swap modes, and re-write
   return;
 }
 
-void set_time(time_struct_t* rtc_time) {
-  return;
+rtc_err_t set_time(time_struct_t* rtc_time) {
+  return RTC_OK;
 }
 
-void get_time(time_struct_t* rtc_time) {
-  return;
+rtc_err_t get_time(time_struct_t* rtc_time) {
+  return RTC_OK;
 }
--- rtc.h
+++ rtc.h
@@ -1,3 +1,8 @@
+/* This library is designed to interface an AVR microcontroller
+ * with a DS1307 RTC chip over the I2C (TWI) interface. It's
+ * primarily designed to use 24-hour mode.
+ */
+
 #ifndef RTC_H
 #define RTC_H
 
@@ -39,21 +44,29 @@
 #define MSK_CTRL_SQWE   0x10
 #define MSK_CTRL_RS     0x30
 
+// error codes
+typedef enum {
+  RTC_OK
+} rtc_err_t;
+
 // we'll use the POSIX ctime broken-down structure
 typedef struct tm time_struct_t;
 
-/* prepare the RTC for use
+/* prepare the AVR to talk to the rtc, and run any setup needed on the RTC
+ * returns: error code or RTC_OK, from rtc_err_t enum
  */
 void rtc_init(void);
 
 /* set the RTC to a specific point in time
  * rtc_time: a pointer to a ctime-style broken down time structure
+ * returns: error code or RTC_OK, from rtc_err_t enum
  */
-void set_time(time_struct_t* rtc_time);
+rtc_err_t set_time(time_struct_t* rtc_time);
 
 /* get the current time from the rtc
  * rtc_time: a pointer to a ctime-style broken down time structure
+ * returns: error code or RTC_OK, from rtc_err_t enum
  */
-void get_time(time_struct_t*);
+rtc_err_t get_time(time_struct_t*);
 
 #endif
